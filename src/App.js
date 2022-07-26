59cf7a5b21d46991c1415f19dc97d7d88d4fa486
hectormamani18 <Hectormamani18@hotmail.com>
2022-07-26 20:21:33 -0300

    Navbar funcional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import './components/Navbar';
+import  {BrowserRouter as Router, Switch, Route}  from 'react-router-dom'
+import Navbar from './components/nav/Navbar'
 
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+      <Navbar/>
+     </Router>
+
+    
     </div>
   );
 }
