2f2a499a1b65dd437271335a9604e0f6daa8b6e4
hectormamani18 <Hectormamani18@hotmail.com>
2022-07-27 22:01:17 -0300

    logo en navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
+import React from 'react';
 import  {BrowserRouter as Router, Switch, Route}  from 'react-router-dom'
 import Navbar from './components/nav/Navbar'
+import Logo from './components/nav/CartWidget';
 
 function App() {
   return (
     <div className="App">
      <Router>
       <Navbar/>
+      <Logo/>
      </Router>
-
-    
-    </div>
+     </div>
   );
 }
 
---
+++ src/components/nav/CartWidget.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+    return(
+
+        <div>
+            <ion-icon name="cart"></ion-icon>
+        </div>
+    )
+}
+
+export default Logo;
